fd698fefc2924c3367e7a9dc3a7b856cbabe692b
sgbasaraner <sarpbasaraner@gmail.com>
2021-11-09 20:03:47 +0300

    add spring boot project

---
+++ src/main/java/io/github/sgbasaraner/funxchange/FunxchangeApplication.java
@@ -0,0 +1,13 @@
+package io.github.sgbasaraner.funxchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunxchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunxchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/sgbasaraner/funxchange/FunxchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sgbasaraner.funxchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunxchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

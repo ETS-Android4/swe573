c093bfc932f7003c9b1aacadc4d66cee8e8c9a81
sgbasaraner <sarpbasaraner@gmail.com>
2021-11-14 13:48:46 +0300

    add welcome screen

---
+++ frontend/funxchange/lib/screens/welcome.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:funxchange/colors.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Enter your username'),
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Enter your password'),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            MaterialButton(
+              height: 50,
+              minWidth: MediaQuery.of(context).size.width,
+              color: FunColor.fulvous,
+              child: const Text('Log in',
+                  style: TextStyle(fontSize: 16.0, color: Colors.white)),
+              onPressed: () {},
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text("or"),
+            const SizedBox(
+              height: 10,
+            ),
+            MaterialButton(
+              height: 50,
+              minWidth: MediaQuery.of(context).size.width,
+              color: FunColor.fulvous,
+              child: const Text('Sign up',
+                  style: TextStyle(fontSize: 16.0, color: Colors.white)),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

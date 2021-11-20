457831d6088a4008e06ba40a471f563ce601d8a0
sgbasaraner <sarpbasaraner@gmail.com>
2021-11-20 14:47:34 +0300

    get user id as param instead

--- frontend/funxchange/lib/components/user_snapshot.dart
+++ frontend/funxchange/lib/components/user_snapshot.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:funxchange/framework/di.dart';
 import 'package:funxchange/models/user.dart';
 
 class UserSnapshot extends StatelessWidget {
-  final Future<User> userFuture;
+  final String userId;
 
-  const UserSnapshot({Key? key, required this.userFuture}) : super(key: key);
+  const UserSnapshot({Key? key, required this.userId}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<User>(
-      future: userFuture,
+      future: DIContainer.singleton.userRepo.fetchUser(userId),
       builder: (ctx, ss) {
         if (!ss.hasData) {
           return const CupertinoActivityIndicator();

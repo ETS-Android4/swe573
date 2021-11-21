a81679e59d2dda987c185cf3aac825e05822b9ad
sgbasaraner <sarpbasaraner@gmail.com>
2021-11-21 13:39:57 +0300

    add mock implementation for notification data

--- frontend/funxchange/lib/mockds/notification.dart
+++ frontend/funxchange/lib/mockds/notification.dart
@@ -1,11 +1,12 @@
 import 'package:funxchange/data_source/notification.dart';
+import 'package:funxchange/mockds/utils.dart';
 import 'package:funxchange/models/notification.dart';
 
 class MockNotificationDataSource implements NotificationDataSource {
+  static List<Notification> data = [];
+
   @override
   Future<List<Notification>> fetchNotifications(int limit, int offset) {
-    // TODO: implement fetchNotifications
-    throw UnimplementedError();
+    return MockUtils.delayed(() => data.skip(offset).take(limit).toList());
   }
-
 }

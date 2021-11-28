e00192954b45e90147ba90ad8e0a33303690aef1
sgbasaraner <sarpbasaraner@gmail.com>
2021-11-28 17:07:10 +0300

    add notif model

---
+++ frontend/funxchange/lib/models/request.dart
@@ -0,0 +1,8 @@
+class JoinRequest {
+  final String id;
+  final String eventId;
+  final String userId;
+  final String htmlText;
+
+  JoinRequest(this.id, this.eventId, this.userId, this.htmlText);
+}

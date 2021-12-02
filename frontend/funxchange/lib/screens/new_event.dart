2c9bc8ece59591101abfa89912892ef2f031f3a7
sgbasaraner <sarpbasaraner@gmail.com>
2021-12-02 21:41:04 +0300

    add new event page - wip

--- frontend/funxchange/lib/screens/new_event.dart
+++ frontend/funxchange/lib/screens/new_event.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:funxchange/components/location_picker.dart';
+import 'package:funxchange/framework/utils.dart';
 
-class NewEventScreen extends StatelessWidget {
+class NewEventScreen extends StatefulWidget {
   const NewEventScreen({Key? key}) : super(key: key);
+
+  @override
+  State<NewEventScreen> createState() => _NewEventScreenState();
+}
+
+class _NewEventScreenState extends State<NewEventScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  DateTime? _startDateTime;
+  DateTime? _endDateTime;
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +24,88 @@ class NewEventScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a title';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Enter title',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some details';
+                      }
+                      return null;
+                    },
+                    maxLines: 4,
+                    minLines: 4,
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Enter details',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
           const LocationPicker(),
+          MaterialButton(
+            child: Text(_startDateTime != null
+                ? Utils.formatDateTime(_startDateTime!)
+                : "Pick Start Time"),
+            onPressed: () {
+              DatePicker.showDateTimePicker(context,
+                  minTime: DateTime.now(),
+                  maxTime: DateTime.now().add(const Duration(days: 364)),
+                  currentTime: DateTime.now(), onConfirm: (d) {
+                setState(() {
+                  _startDateTime = d;
+                });
+              });
+            },
+          ),
+          MaterialButton(
+            child: Text(_endDateTime != null
+                ? Utils.formatDateTime(_endDateTime!)
+                : "Pick End Time"),
+            onPressed: () {
+              DatePicker.showDateTimePicker(context,
+                  minTime: _startDateTime ?? DateTime.now(),
+                  maxTime: DateTime.now().add(const Duration(days: 365)),
+                  currentTime: _startDateTime ?? DateTime.now(),
+                  onConfirm: (d) {
+                setState(() {
+                  _endDateTime = d;
+                });
+              });
+            },
+          ),
+          MaterialButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Processing Data')),
+                );
+                return;
+              }
+            },
+            child: const Text('Submit'),
+          )
         ],
       ),
     );

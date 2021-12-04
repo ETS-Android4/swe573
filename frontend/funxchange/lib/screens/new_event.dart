79b7693ed7b1d8da4de6eadeb4a7dc4dc93e9f5f
sgbasaraner <sarpbasaraner@gmail.com>
2021-12-04 14:31:49 +0300

    add category picker

--- frontend/funxchange/lib/screens/new_event.dart
+++ frontend/funxchange/lib/screens/new_event.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:funxchange/components/location_picker.dart';
+import 'package:funxchange/framework/colors.dart';
 import 'package:funxchange/framework/utils.dart';
 import 'package:funxchange/models/event.dart';
+import 'package:funxchange/models/interest.dart';
 
 class NewEventScreen extends StatefulWidget {
   const NewEventScreen({Key? key}) : super(key: key);
@@ -17,6 +19,8 @@ class _NewEventScreenState extends State<NewEventScreen> {
   DateTime? _startDateTime;
   DateTime? _endDateTime;
 
+  Interest _currentCategory = Interest.golf;
+
   var isSelected = [true, false];
 
   EventType _currentEventType = EventType.service;
@@ -24,6 +28,12 @@ class _NewEventScreenState extends State<NewEventScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          // TODO: submit
+        },
+      ),
       appBar: AppBar(
         title: const Text("New Event"),
       ),
@@ -105,37 +115,90 @@ class _NewEventScreenState extends State<NewEventScreen> {
               ],
             ),
           ),
-          const LocationPicker(),
-          MaterialButton(
-            child: Text(_startDateTime != null
-                ? Utils.formatDateTime(_startDateTime!)
-                : "Pick Start Time"),
-            onPressed: () {
-              DatePicker.showDateTimePicker(context,
-                  minTime: DateTime.now(),
-                  maxTime: DateTime.now().add(const Duration(days: 364)),
-                  currentTime: DateTime.now(), onConfirm: (d) {
-                setState(() {
-                  _startDateTime = d;
-                });
-              });
-            },
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const Text("Category: "),
+              DropdownButton<String>(
+                value: _currentCategory.prettyName,
+                icon: const Icon(Icons.arrow_downward),
+                iconSize: 24,
+                elevation: 16,
+                style: const TextStyle(color: FunColor.fulvous),
+                underline: Container(
+                  height: 2,
+                  color: FunColor.fulvous,
+                ),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    _currentCategory = Interest.values.firstWhere(
+                        (element) => element.prettyName == newValue!);
+                  });
+                },
+                items: Interest.values
+                    .map((e) => e.prettyName)
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ],
           ),
-          MaterialButton(
-            child: Text(_endDateTime != null
-                ? Utils.formatDateTime(_endDateTime!)
-                : "Pick End Time"),
-            onPressed: () {
-              DatePicker.showDateTimePicker(context,
-                  minTime: _startDateTime ?? DateTime.now(),
-                  maxTime: DateTime.now().add(const Duration(days: 365)),
-                  currentTime: _startDateTime ?? DateTime.now(),
-                  onConfirm: (d) {
-                setState(() {
-                  _endDateTime = d;
-                });
-              });
-            },
+          const LocationPicker(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const Text(
+                "Start Time: ",
+                style:
+                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              MaterialButton(
+                child: Text(
+                  _startDateTime != null
+                      ? Utils.formatDateTime(_startDateTime!)
+                      : "Pick Start Time",
+                ),
+                onPressed: () {
+                  DatePicker.showDateTimePicker(context,
+                      minTime: DateTime.now(),
+                      maxTime: DateTime.now().add(const Duration(days: 364)),
+                      currentTime: DateTime.now(), onConfirm: (d) {
+                    setState(() {
+                      _startDateTime = d;
+                    });
+                  });
+                },
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const Text(
+                "End Time: ",
+                style:
+                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              MaterialButton(
+                child: Text(_endDateTime != null
+                    ? Utils.formatDateTime(_endDateTime!)
+                    : "Pick End Time"),
+                onPressed: () {
+                  DatePicker.showDateTimePicker(context,
+                      minTime: _startDateTime ?? DateTime.now(),
+                      maxTime: DateTime.now().add(const Duration(days: 365)),
+                      currentTime: _startDateTime ?? DateTime.now(),
+                      onConfirm: (d) {
+                    setState(() {
+                      _endDateTime = d;
+                    });
+                  });
+                },
+              ),
+            ],
           )
         ],
       ),

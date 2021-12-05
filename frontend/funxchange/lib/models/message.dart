6c521ec9076655ff4de1833bd1e6bd6ac0af8aa7
sgbasaraner <sarpbasaraner@gmail.com>
2021-12-05 18:06:57 +0300

    add message model

---
+++ frontend/funxchange/lib/models/message.dart
@@ -0,0 +1,8 @@
+class Message {
+  final String senderId;
+  final String senderUserName;
+  final String text;
+  final DateTime created;
+
+  Message(this.senderId, this.senderUserName, this.text, this.created);
+}

2adc7b61f023875d0a97ec029a58cf56ed1ef35e
sgbasaraner <sarpbasaraner@gmail.com>
2021-12-08 22:16:13 +0300

    implement controller

--- backend/funxchange/src/main/java/io/github/sgbasaraner/funxchange/controller/UserController.java
+++ backend/funxchange/src/main/java/io/github/sgbasaraner/funxchange/controller/UserController.java
@@ -2,6 +2,8 @@ package io.github.sgbasaraner.funxchange.controller;
 
 import io.github.sgbasaraner.funxchange.model.NewUserDTO;
 import io.github.sgbasaraner.funxchange.model.UserDTO;
+import io.github.sgbasaraner.funxchange.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,8 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    @Autowired
+    private UserService service;
+
     @PostMapping("/signup")
     public ResponseEntity<UserDTO> signUp(@RequestBody NewUserDTO params) {
-        return null;
+        try {
+            return ResponseEntity.ok(service.signUp(params));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }

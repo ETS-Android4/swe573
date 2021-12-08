10457a7455c64d17c7478e4d958ae3e12fc3be2e
sgbasaraner <sarpbasaraner@gmail.com>
2021-12-08 21:27:07 +0300

    remove test classes

--- backend/funxchange/src/main/java/io/github/sgbasaraner/funxchange/FunxchangeApplication.java
+++ backend/funxchange/src/main/java/io/github/sgbasaraner/funxchange/FunxchangeApplication.java
@@ -1,7 +1,5 @@
 package io.github.sgbasaraner.funxchange;
 
-import io.github.sgbasaraner.funxchange.entity.Test;
-import io.github.sgbasaraner.funxchange.repository.TestRepository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,14 +14,7 @@ import java.util.List;
 @SpringBootApplication
 public class FunxchangeApplication {
 
-	private final TestRepository testRepository;
-
 	Logger logger = LoggerFactory.getLogger(FunxchangeApplication.class);
-
-	@Autowired
-	public FunxchangeApplication(TestRepository testRepository) {
-		this.testRepository = testRepository;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FunxchangeApplication.class, args);
@@ -31,16 +22,7 @@ public class FunxchangeApplication {
 
 	@EventListener(ApplicationReadyEvent.class)
 	public void runAfterStartup() {
-		List<Test> allEntities = testRepository.findAll();
-		logger.info("Number of entities: " + allEntities.size());
 
-		Test testEntity = new Test();
-		testEntity.setTestValue("test value");
-		logger.info("Saving test entity...");
-		testRepository.save(testEntity);
-
-		allEntities = testRepository.findAll();
-		logger.info("Number of entities: " + allEntities.size());
 	}
 
 }

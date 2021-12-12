132c1a62fd1b9df0b580ea690e3ec893a32dea34
sgbasaraner <sarpbasaraner@gmail.com>
2021-12-12 17:05:57 +0300

    add follow service stubs

---
+++ backend/funxchange/src/main/java/io/github/sgbasaraner/funxchange/service/FollowService.java
@@ -0,0 +1,14 @@
+package io.github.sgbasaraner.funxchange.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FollowService {
+    public String followUser(String userName) {
+        return null;
+    }
+
+    public String unfollowUser(String userName) {
+        return null;
+    }
+}

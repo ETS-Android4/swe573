6963e3ba094028f66c9a989af70f83aaecbc0eb1
sgbasaraner <sarpbasaraner@gmail.com>
2021-12-12 17:57:37 +0300

    add integration tests for follow and unfollow methods

--- backend/funxchange/src/main/java/io/github/sgbasaraner/funxchange/repository/FollowerRepository.java
+++ backend/funxchange/src/main/java/io/github/sgbasaraner/funxchange/repository/FollowerRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FollowerRepository extends JpaRepository<Long, Follower> {
+public interface FollowerRepository extends JpaRepository<Follower, Long> {
 }
--- backend/funxchange/src/test/java/io/github/sgbasaraner/funxchange/UserServiceIntegrationTest.java
+++ backend/funxchange/src/test/java/io/github/sgbasaraner/funxchange/UserServiceIntegrationTest.java
@@ -1,9 +1,11 @@
 package io.github.sgbasaraner.funxchange;
 
+import io.github.sgbasaraner.funxchange.entity.Follower;
 import io.github.sgbasaraner.funxchange.entity.Interest;
 import io.github.sgbasaraner.funxchange.entity.User;
 import io.github.sgbasaraner.funxchange.model.NewUserDTO;
 import io.github.sgbasaraner.funxchange.model.UserDTO;
+import io.github.sgbasaraner.funxchange.repository.FollowerRepository;
 import io.github.sgbasaraner.funxchange.repository.InterestRepository;
 import io.github.sgbasaraner.funxchange.repository.UserRepository;
 import io.github.sgbasaraner.funxchange.service.UserDetailsServiceImpl;
@@ -18,14 +20,11 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
@@ -70,6 +69,9 @@ public class UserServiceIntegrationTest {
     @MockBean
     private InterestRepository interestRepository;
 
+    @MockBean
+    private FollowerRepository followRepository;
+
     @Test
     public void whenValidUser_thenUserShouldBeSaved() {
         User user = new User();
@@ -83,5 +85,40 @@ public class UserServiceIntegrationTest {
         NewUserDTO newUserParams = new NewUserDTO(user.getUserName(), user.getBio(), List.of(interest.getName()), "somelongpassword");
         UserDTO createdUser = userService.signUp(newUserParams);
         Assertions.assertEquals(createdUser.getId(), user.getId().toString());
+    }
+
+    @Test
+    public void whenFollowed_followShouldBeSaved() {
+        final User currentUser = new User();
+        currentUser.setUserName("test_user");
+        currentUser.setId(UUID.randomUUID());
+
+
+        final User userToFollow = new User();
+        userToFollow.setUserName("followed_user");
+        userToFollow.setId(UUID.randomUUID());
+
+        final Follower followEntity = new Follower();
+        followEntity.setFollower(currentUser);
+        followEntity.setFollowee(userToFollow);
+
+        Mockito.when(followRepository.save(Mockito.any(Follower.class))).thenReturn(followEntity);
+        final String followedUserId = userService.followUser(userToFollow.getId().toString(), currentUser::getUserName);
+        Assertions.assertEquals(userToFollow.getId().toString(), followedUserId);
+    }
+
+    @Test
+    public void whenUnfollowed_followShouldBeDeleted() {
+        final User currentUser = new User();
+        currentUser.setUserName("test_user");
+        currentUser.setId(UUID.randomUUID());
+
+        final User userToUnfollow = new User();
+        userToUnfollow.setUserName("unfollowed_user");
+        userToUnfollow.setId(UUID.randomUUID());
+
+        Mockito.verify(followRepository).delete(Mockito.any(Follower.class));
+        final String followedUserId = userService.followUser(userToUnfollow.getId().toString(), currentUser::getUserName);
+        Assertions.assertEquals(userToUnfollow.getId().toString(), followedUserId);
     }
 }

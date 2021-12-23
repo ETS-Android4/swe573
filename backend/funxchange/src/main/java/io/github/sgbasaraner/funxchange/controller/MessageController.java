497a0887f905de0bf27f99f949d31c17269d6a15
sgbasaraner <sarpbasaraner@gmail.com>
2021-12-23 21:41:31 +0300

    add controller methods

--- backend/funxchange/src/main/java/io/github/sgbasaraner/funxchange/controller/MessageController.java
+++ backend/funxchange/src/main/java/io/github/sgbasaraner/funxchange/controller/MessageController.java
@@ -2,6 +2,8 @@ package io.github.sgbasaraner.funxchange.controller;
 
 import io.github.sgbasaraner.funxchange.model.MessageDTO;
 import io.github.sgbasaraner.funxchange.model.NewMessageDTO;
+import io.github.sgbasaraner.funxchange.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -9,18 +11,22 @@ import java.util.List;
 
 @RestController
 public class MessageController {
+
+    @Autowired
+    private MessageService service;
+
     @GetMapping("/conversations")
     List<MessageDTO> fetchConversations(Principal principal, @RequestParam int offset, @RequestParam int limit) {
-        return null;
+        return service.fetchConversations(principal, offset, limit);
     }
 
     @GetMapping("/conversations/{conversationId}/messages")
     List<MessageDTO> fetchMessages(Principal principal, @RequestParam int offset, @RequestParam int limit, @PathVariable String conversationId) {
-        return null;
+        return service.fetchMessages(principal, offset, limit, conversationId);
     }
 
     @PostMapping("/conversations")
     MessageDTO sendMessage(Principal principal, @RequestBody NewMessageDTO message) {
-        return null;
+        return service.sendMessage(principal, message);
     }
 }

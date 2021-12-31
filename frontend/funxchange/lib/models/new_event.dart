e8edd0a4f726810dc08d216cf5df37c09da91901
sgbasaraner <sarpbasaraner@gmail.com>
2021-12-31 16:58:07 +0300

    parse new event params

--- frontend/funxchange/lib/models/new_event.dart
+++ frontend/funxchange/lib/models/new_event.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:funxchange/models/event.dart';
 import 'package:funxchange/models/interest.dart';
 
@@ -27,4 +29,41 @@ class NewEventParams {
     this.durationInMinutes,
     this.dateTime,
   );
+
+  Map<String, dynamic> toMap() {
+    return {
+      'type': type.toString(),
+      'capacity': capacity,
+      'category': category.toString(),
+      'title': title,
+      'details': details,
+      'latitude': latitude,
+      'longitude': longitude,
+      'cityName': cityName,
+      'countryName': countryName,
+      'durationInMinutes': durationInMinutes,
+      'dateTime': dateTime.millisecondsSinceEpoch,
+    };
+  }
+
+  factory NewEventParams.fromMap(Map<String, dynamic> map) {
+    return NewEventParams(
+      parseEventType(map['type'])!,
+      map['capacity']?.toInt() ?? 0,
+      parseInterest(map['category'])!,
+      map['title'] ?? '',
+      map['details'] ?? '',
+      map['latitude']?.toDouble() ?? 0.0,
+      map['longitude']?.toDouble() ?? 0.0,
+      map['cityName'] ?? '',
+      map['countryName'] ?? '',
+      map['durationInMinutes']?.toInt() ?? 0,
+      DateTime.fromMillisecondsSinceEpoch(map['dateTime']),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory NewEventParams.fromJson(String source) =>
+      NewEventParams.fromMap(json.decode(source));
 }

a24b67f1108bb95834d85f987d2724db46b032ed
sgbasaraner <sarpbasaraner@gmail.com>
2021-12-31 16:52:39 +0300

    add cs model

--- frontend/funxchange/lib/models/credit_score.dart
+++ frontend/funxchange/lib/models/credit_score.dart
@@ -1,4 +1,26 @@
+import 'dart:convert';
+
 class CreditScore {
   final int creditScore;
   final int creditOnHold;
+
+  CreditScore(this.creditScore, this.creditOnHold);
+
+  Map<String, dynamic> toMap() {
+    return {
+      'creditScore': creditScore,
+      'creditOnHold': creditOnHold,
+    };
+  }
+
+  factory CreditScore.fromMap(Map<String, dynamic> map) {
+    return CreditScore(
+      map['creditScore']?.toInt() ?? 0,
+      map['creditOnHold']?.toInt() ?? 0,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory CreditScore.fromJson(String source) => CreditScore.fromMap(json.decode(source));
 }

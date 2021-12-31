0718764c5b1c27172a0932c13095d532004d4095
sgbasaraner <sarpbasaraner@gmail.com>
2021-12-31 16:45:47 +0300

    implement controller layer

--- backend/funxchange/src/main/java/io/github/sgbasaraner/funxchange/controller/RatingController.java
+++ backend/funxchange/src/main/java/io/github/sgbasaraner/funxchange/controller/RatingController.java
@@ -1,6 +1,8 @@
 package io.github.sgbasaraner.funxchange.controller;
 
 import io.github.sgbasaraner.funxchange.model.RatingDTO;
+import io.github.sgbasaraner.funxchange.service.RatingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -8,13 +10,17 @@ import java.util.List;
 
 @RestController
 public class RatingController {
+
+    @Autowired
+    private RatingService service;
+
     @GetMapping("/ratings")
     List<RatingDTO> fetchRatings(Principal principal, @RequestParam int offset, @RequestParam int limit) {
-        return null;
+        return service.fetchRatings(principal, offset, limit);
     }
 
     @PostMapping("/ratings")
     RatingDTO updateRating(Principal principal, @RequestBody RatingDTO ratingDTO) {
-        return null;
+        return service.updateRating(principal, ratingDTO);
     }
 }

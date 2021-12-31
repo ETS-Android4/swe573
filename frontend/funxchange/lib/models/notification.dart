69c96808e2a84af3b822cea1a6f0a66e4eeee815
sgbasaraner <sarpbasaraner@gmail.com>
2021-12-31 16:58:25 +0300

    parse notification model

--- frontend/funxchange/lib/models/notification.dart
+++ frontend/funxchange/lib/models/notification.dart
@@ -1,6 +1,27 @@
+import 'dart:convert';
+
 class NotificationModel {
   final String htmlText;
   final String deeplink;
 
   NotificationModel(this.htmlText, this.deeplink);
+
+  Map<String, dynamic> toMap() {
+    return {
+      'htmlText': htmlText,
+      'deeplink': deeplink,
+    };
+  }
+
+  factory NotificationModel.fromMap(Map<String, dynamic> map) {
+    return NotificationModel(
+      map['htmlText'] ?? '',
+      map['deeplink'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory NotificationModel.fromJson(String source) =>
+      NotificationModel.fromMap(json.decode(source));
 }

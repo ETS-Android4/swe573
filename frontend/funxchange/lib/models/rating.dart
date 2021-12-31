81b98b162ec4c4c6df8f8a08732d57a8a10490b0
sgbasaraner <sarpbasaraner@gmail.com>
2021-12-31 17:01:41 +0300

    prepare rating model

---
+++ frontend/funxchange/lib/models/rating.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:funxchange/models/event.dart';
+import 'package:funxchange/models/user.dart';
+
+class Rating {
+  final String id;
+  final User user;
+  final int? rating;
+  final Event event;
+
+  Rating(this.id, this.user, this.rating, this.event);
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'user': user.toMap(),
+      'rating': rating,
+      'event': event.toMap(),
+    };
+  }
+
+  factory Rating.fromMap(Map<String, dynamic> map) {
+    return Rating(
+      map['id'] ?? '',
+      User.fromMap(map['user']),
+      map['rating']?.toInt(),
+      Event.fromMap(map['event']),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Rating.fromJson(String source) => Rating.fromMap(json.decode(source));
+}

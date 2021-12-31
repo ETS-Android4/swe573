845a2bed484f4149380cfa5b91384ad146635dd6
sgbasaraner <sarpbasaraner@gmail.com>
2021-12-31 17:00:31 +0300

    parse user model

--- frontend/funxchange/lib/models/user.dart
+++ frontend/funxchange/lib/models/user.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:funxchange/models/credit_score.dart';
 import 'package:funxchange/models/interest.dart';
 
@@ -12,5 +14,40 @@ class User {
   final double ratingAvg;
   bool? isFollowed;
 
-  User(this.id, this.userName, this.bio, this.followerCount, this.followedCount, this.interests, this.isFollowed, this.creditScore, this.ratingAvg);
+  User(this.id, this.userName, this.bio, this.followerCount, this.followedCount,
+      this.interests, this.isFollowed, this.creditScore, this.ratingAvg);
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'userName': userName,
+      'bio': bio,
+      'followerCount': followerCount,
+      'followedCount': followedCount,
+      'interests': interests.map((x) => x.toString()).toList(),
+      'creditScore': creditScore?.toMap(),
+      'ratingAvg': ratingAvg,
+      'isFollowed': isFollowed,
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      map['id'] ?? '',
+      map['userName'] ?? '',
+      map['bio'] ?? '',
+      map['followerCount']?.toInt() ?? 0,
+      map['followedCount']?.toInt() ?? 0,
+      List<Interest>.from(map['interests']?.map((x) => parseInterest(x)!)),
+      map['isFollowed'],
+      map['creditScore'] != null
+          ? CreditScore.fromMap(map['creditScore'])
+          : null,
+      map['ratingAvg']?.toDouble() ?? 0.0,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory User.fromJson(String source) => User.fromMap(json.decode(source));
 }

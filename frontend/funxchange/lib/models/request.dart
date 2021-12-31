77dac630bca3e04d54b9e347beea3832dc2a8171
sgbasaraner <sarpbasaraner@gmail.com>
2021-12-31 16:58:39 +0300

    parse join request model

--- frontend/funxchange/lib/models/request.dart
+++ frontend/funxchange/lib/models/request.dart
@@ -1,7 +1,30 @@
+import 'dart:convert';
+
 class JoinRequest {
   final String eventId;
   final String userId;
   final String htmlText;
 
   JoinRequest(this.eventId, this.userId, this.htmlText);
+
+  Map<String, dynamic> toMap() {
+    return {
+      'eventId': eventId,
+      'userId': userId,
+      'htmlText': htmlText,
+    };
+  }
+
+  factory JoinRequest.fromMap(Map<String, dynamic> map) {
+    return JoinRequest(
+      map['eventId'] ?? '',
+      map['userId'] ?? '',
+      map['htmlText'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory JoinRequest.fromJson(String source) =>
+      JoinRequest.fromMap(json.decode(source));
 }

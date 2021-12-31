c04827a52b53eb5c41a5c79e7445fd72764563cc
sgbasaraner <sarpbasaraner@gmail.com>
2021-12-31 16:45:06 +0300

    implement rating service

--- backend/funxchange/src/main/java/io/github/sgbasaraner/funxchange/repository/RatingRepository.java
+++ backend/funxchange/src/main/java/io/github/sgbasaraner/funxchange/repository/RatingRepository.java
@@ -1,11 +1,15 @@
 package io.github.sgbasaraner.funxchange.repository;
 
 import io.github.sgbasaraner.funxchange.entity.Rating;
+import io.github.sgbasaraner.funxchange.entity.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface RatingRepository extends JpaRepository<Rating, UUID> {
+    List<Rating> findByRater(User rater, Pageable pageable);
 }
--- backend/funxchange/src/main/java/io/github/sgbasaraner/funxchange/service/RatingService.java
+++ backend/funxchange/src/main/java/io/github/sgbasaraner/funxchange/service/RatingService.java
@@ -5,11 +5,17 @@ import io.github.sgbasaraner.funxchange.entity.User;
 import io.github.sgbasaraner.funxchange.model.RatingDTO;
 import io.github.sgbasaraner.funxchange.repository.RatingRepository;
 import io.github.sgbasaraner.funxchange.repository.UserRepository;
+import io.github.sgbasaraner.funxchange.util.Util;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.security.Principal;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class RatingService {
@@ -26,12 +32,29 @@ public class RatingService {
     @Autowired
     private EventService eventService;
 
+    @Autowired
+    private Util util;
+
+    @Transactional
     public List<RatingDTO> fetchRatings(Principal principal, int offset, int limit) {
-        return null;
+        final User requestor = userRepository.findUserByUserName(principal.getName()).get();
+        final Pageable page = util.makePageable(offset, limit, Sort.by("created").descending());
+        return ratingRepository.findByRater(requestor, page)
+                .stream()
+                .map(r -> mapToDTO(r, requestor))
+                .collect(Collectors.toUnmodifiableList());
     }
 
+    @Transactional
     public RatingDTO updateRating(Principal principal, RatingDTO ratingDTO) {
-        return null;
+        final User requestor = userRepository.findUserByUserName(principal.getName()).get();
+        final Rating existingRating = ratingRepository.getById(UUID.fromString(ratingDTO.getId()));
+        if (!existingRating.getRater().getId().equals(requestor.getId()))
+            throw new IllegalArgumentException("Invalid rating");
+
+        existingRating.setRating(ratingDTO.getRating());
+
+        return mapToDTO(ratingRepository.save(existingRating), requestor);
     }
 
     private RatingDTO mapToDTO(Rating entity, User requestor) {

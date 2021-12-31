968eaecaa621922424afe0883b305f862a313bea
sgbasaraner <sarpbasaraner@gmail.com>
2021-12-31 16:57:30 +0300

    parse message model

--- frontend/funxchange/lib/models/message.dart
+++ frontend/funxchange/lib/models/message.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Message {
   final String senderId;
   final String receiverId;
@@ -16,4 +18,33 @@ class Message {
     this.text,
     this.created,
   );
+
+  Map<String, dynamic> toMap() {
+    return {
+      'senderId': senderId,
+      'receiverId': receiverId,
+      'conversationId': conversationId,
+      'senderUserName': senderUserName,
+      'receiverUserName': receiverUserName,
+      'text': text,
+      'created': created.millisecondsSinceEpoch,
+    };
+  }
+
+  factory Message.fromMap(Map<String, dynamic> map) {
+    return Message(
+      map['senderId'] ?? '',
+      map['receiverId'] ?? '',
+      map['conversationId'] ?? '',
+      map['senderUserName'] ?? '',
+      map['receiverUserName'] ?? '',
+      map['text'] ?? '',
+      DateTime.fromMillisecondsSinceEpoch(map['created']),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Message.fromJson(String source) =>
+      Message.fromMap(json.decode(source));
 }

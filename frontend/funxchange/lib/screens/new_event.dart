afea77e7705e5e398fff4463e355f9ba9a8f9afb
sgbasaraner <sarpbasaraner@gmail.com>
2022-01-08 16:27:44 +0300

    wrap layout inside a single child scroll view to avoid issues when keyboard appears

--- frontend/funxchange/lib/screens/new_event.dart
+++ frontend/funxchange/lib/screens/new_event.dart
@@ -101,196 +101,198 @@ class _NewEventScreenState extends State<NewEventScreen> {
       appBar: AppBar(
         title: const Text("New Event"),
       ),
-      body: Column(
-        children: [
-          Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ToggleButtons(
-                    children: const [
-                      Text("        Service        "),
-                      Text("        Meetup        "),
-                    ],
-                    onPressed: (int index) {
-                      setState(() {
-                        isSelected[0] = !isSelected[0];
-                        isSelected[1] = !isSelected[1];
-                        if (index == 0) {
-                          _currentEventType = EventType.service;
-                        } else {
-                          _currentEventType = EventType.meetup;
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ToggleButtons(
+                      children: const [
+                        Text("        Service        "),
+                        Text("        Meetup        "),
+                      ],
+                      onPressed: (int index) {
+                        setState(() {
+                          isSelected[0] = !isSelected[0];
+                          isSelected[1] = !isSelected[1];
+                          if (index == 0) {
+                            _currentEventType = EventType.service;
+                          } else {
+                            _currentEventType = EventType.meetup;
+                          }
+                        });
+                      },
+                      isSelected: isSelected,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    child: TextFormField(
+                      controller: _titleController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a title';
                         }
-                      });
-                    },
-                    isSelected: isSelected,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                  child: TextFormField(
-                    controller: _titleController,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a title';
-                      }
 
-                      if (value.length > 255) {
-                        return 'Title must contain at most 255 characters.';
-                      }
-                      return null;
-                    },
-                    decoration: const InputDecoration(
-                      border: UnderlineInputBorder(),
-                      labelText: 'Enter title',
+                        if (value.length > 255) {
+                          return 'Title must contain at most 255 characters.';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Enter title',
+                      ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                  child: TextFormField(
-                    controller: _detailController,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some details';
-                      }
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    child: TextFormField(
+                      controller: _detailController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some details';
+                        }
 
-                      const limit = 255 * 8;
+                        const limit = 255 * 8;
 
-                      if (value.length > limit) {
-                        return 'Title must contain at most ' +
-                            limit.toString() +
-                            ' characters.';
-                      }
-                      return null;
-                    },
-                    maxLines: 4,
-                    minLines: 4,
-                    decoration: const InputDecoration(
-                      border: UnderlineInputBorder(),
-                      labelText: 'Enter details',
+                        if (value.length > limit) {
+                          return 'Title must contain at most ' +
+                              limit.toString() +
+                              ' characters.';
+                        }
+                        return null;
+                      },
+                      maxLines: 4,
+                      minLines: 4,
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Enter details',
+                      ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                  child: TextFormField(
-                    controller: _participantCountController,
-                    keyboardType: TextInputType.number,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter participant count';
-                      }
-                      int? intVal = int.tryParse(value);
-                      if (intVal == null) {
-                        return 'Please enter a number.';
-                      }
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    child: TextFormField(
+                      controller: _participantCountController,
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter participant count';
+                        }
+                        int? intVal = int.tryParse(value);
+                        if (intVal == null) {
+                          return 'Please enter a number.';
+                        }
 
-                      if (intVal < 1 || intVal > 12) {
-                        return 'Participant count must be between 1 and 12.';
-                      }
+                        if (intVal < 1 || intVal > 12) {
+                          return 'Participant count must be between 1 and 12.';
+                        }
 
-                      return null;
-                    },
-                    decoration: const InputDecoration(
-                      border: UnderlineInputBorder(),
-                      labelText: 'Enter participant count',
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Enter participant count',
+                      ),
                     ),
                   ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Text("Category: "),
+                DropdownButton<String>(
+                  value: _currentCategory.prettyName,
+                  icon: const Icon(Icons.arrow_downward),
+                  iconSize: 24,
+                  elevation: 16,
+                  style: const TextStyle(color: FunColor.fulvous),
+                  underline: Container(
+                    height: 2,
+                    color: FunColor.fulvous,
+                  ),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _currentCategory = Interest.values.firstWhere(
+                          (element) => element.prettyName == newValue!);
+                    });
+                  },
+                  items: Interest.values
+                      .map((e) => e.prettyName)
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
                 ),
               ],
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              const Text("Category: "),
-              DropdownButton<String>(
-                value: _currentCategory.prettyName,
-                icon: const Icon(Icons.arrow_downward),
-                iconSize: 24,
-                elevation: 16,
-                style: const TextStyle(color: FunColor.fulvous),
-                underline: Container(
-                  height: 2,
-                  color: FunColor.fulvous,
+            LocationPicker(
+              onLocationPicked: (loc) {
+                _currentLocation = loc;
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Text(
+                  "Start Time: ",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    _currentCategory = Interest.values.firstWhere(
-                        (element) => element.prettyName == newValue!);
-                  });
-                },
-                items: Interest.values
-                    .map((e) => e.prettyName)
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
-            ],
-          ),
-          LocationPicker(
-            onLocationPicked: (loc) {
-              _currentLocation = loc;
-            },
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              const Text(
-                "Start Time: ",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              MaterialButton(
-                child: Text(
-                  _startDateTime != null
-                      ? Utils.formatDateTime(_startDateTime!)
-                      : "Pick Start Time",
+                MaterialButton(
+                  child: Text(
+                    _startDateTime != null
+                        ? Utils.formatDateTime(_startDateTime!)
+                        : "Pick Start Time",
+                  ),
+                  onPressed: () {
+                    DatePicker.showDateTimePicker(context,
+                        minTime: DateTime.now(),
+                        maxTime: DateTime.now().add(const Duration(days: 364)),
+                        currentTime: DateTime.now(), onConfirm: (d) {
+                      setState(() {
+                        _startDateTime = d;
+                      });
+                    });
+                  },
                 ),
-                onPressed: () {
-                  DatePicker.showDateTimePicker(context,
-                      minTime: DateTime.now(),
-                      maxTime: DateTime.now().add(const Duration(days: 364)),
-                      currentTime: DateTime.now(), onConfirm: (d) {
-                    setState(() {
-                      _startDateTime = d;
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Text(
+                  "End Time: ",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                MaterialButton(
+                  child: Text(_endDateTime != null
+                      ? Utils.formatDateTime(_endDateTime!)
+                      : "Pick End Time"),
+                  onPressed: () {
+                    DatePicker.showDateTimePicker(context,
+                        minTime: _startDateTime ?? DateTime.now(),
+                        maxTime: DateTime.now().add(const Duration(days: 365)),
+                        currentTime: _startDateTime ?? DateTime.now(),
+                        onConfirm: (d) {
+                      setState(() {
+                        _endDateTime = d;
+                      });
                     });
-                  });
-                },
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              const Text(
-                "End Time: ",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              MaterialButton(
-                child: Text(_endDateTime != null
-                    ? Utils.formatDateTime(_endDateTime!)
-                    : "Pick End Time"),
-                onPressed: () {
-                  DatePicker.showDateTimePicker(context,
-                      minTime: _startDateTime ?? DateTime.now(),
-                      maxTime: DateTime.now().add(const Duration(days: 365)),
-                      currentTime: _startDateTime ?? DateTime.now(),
-                      onConfirm: (d) {
-                    setState(() {
-                      _endDateTime = d;
-                    });
-                  });
-                },
-              ),
-            ],
-          )
-        ],
+                  },
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
